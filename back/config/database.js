34609aeb28a4f6ed5f299727f4b3148096231a86
Leydi Vidal <afroladymersi2012@gmail.com>
2022-10-08 18:55:01 -0500

    Base de datos Mongo - localhost
    La base de datos mongo se conectó con el servidor: local host

---
+++ back/config/database.js
@@ -0,0 +1,13 @@
+const mongoose=require("mongoose");
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_LOCAL_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(con => {
+        console.log(`Base de datos mongo conectada con el servidor: ${con.connection.host}`)
+
+    })
+}
+  
+module.exports =connectDatabase; //Para utilizar esta conexion a la base de datos se debe exportar el module.
--- back/server.js
+++ back/server.js
@@ -1,9 +1,15 @@
-const app=require("./app")   
+const app=require("./app")  
+const connectDatabase = require("./config/database");
 
 //setear el archivo de configuración. 
 const dotenv = require("dotenv");
 dotenv.config({path: 'back/config/config.env'})
 
+//Configurar la base de datos. 
+connectDatabase();
+
+
+//Llamamos al server.
 const server=app.listen(process.env.PORT, () => {
     console.log(`Servidor iniciando en el puerto: ${process.env.PORT} en modo: ${process.env.NODE_ENV}`)
 })
