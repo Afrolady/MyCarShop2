0d8b431e4e287c052f9399fd3eed61d80841ba56
Leydi Vidal <afroladymersi2012@gmail.com>
2022-10-16 09:39:57 -0500

    Descripción del producto
    En el Postman se creó un archivo Json realizando la descripción de los productos a vender el cual se reflejo en MongoDB.

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,7 +1,102 @@
-exports.getProducts=(req,res,next) => { //puede pedir o requerir algo, responder a algo puede ejecute una accion al terminar con next. 
-    res.status(200).json({
-        success: true,
-        message: "En esta ruta usted va a poder ver todos los productos"
-    })
+const producto=require("../models/productos")
+const fetch =(url)=>import('node-fetch').then(({default:fetch})=>fetch(url)); //Usurpación del require
 
+//Ver la lista de productos
+exports.getProducts=async (req,res,next) =>{
+    const productos= await producto.find();
+    res.status(200).json({
+        success:true,
+        cantidad: productos.length,
+        productos
+    })
 }
+
+//Ver un producto por ID
+exports.getProductById= async (req, res, next)=>{
+    const product= await producto.findById(req.params.id)
+    
+    if (!product){
+            return res.status(404).json({
+            success:false,
+            message: 'No encontramos ese producto'
+        })
+    }
+    res.status(200).json({
+        success:true,
+        message:"Aqui debajo encuentras información sobre tu producto: ",
+        product
+    })
+}
+
+//Update un producto
+exports.updateProduct= async (req,res,next) =>{
+    let product= await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product){ //Verifico que el objeto no existe para finalizar el proceso
+            return res.status(404).json({
+            success:false,
+            message: 'No encontramos ese producto'
+        })
+    }
+    //Si el objeto si existia, entonces si ejecuto la actualización
+    product= await producto.findByIdAndUpdate(req.params.id, req.body, {
+        new:true, //Valido solo los atributos nuevos o actualizados
+        runValidators:true
+    });
+    //Respondo Ok si el producto si se actualizó
+    res.status(200).json({
+        success:true,
+        message:"Producto actualizado correctamente",
+        product
+    })
+}
+
+
+//Eliminar un producto
+exports.deleteProduct= async (req,res,next) =>{
+    const product= await producto.findById(req.params.id) //Variable de tipo modificable
+    if (!product){ //Verifico que el objeto no existe para finalizar el proceso
+            return res.status(404).json({ //Si el objeto no existe, return termina el metodo
+            success:false,
+            message: 'No encontramos ese producto'
+        })
+    }
+
+    await product.remove();//Eliminamos el proceso
+    res.status(200).json({
+        success:true,
+        message:"Producto eliminado correctamente"
+    })
+}
+
+//Crear nuevo producto /api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product= await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
+    })
+}
+
+
+//HABLEMOS DE FETCH
+//Ver todos los productos
+function verProductos(){
+    fetch('http://localhost:4000/api/productos')
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductos(); LLamamos al metodo creado para probar la consulta
+
+//Ver por id
+function verProductoPorID(id){
+    fetch('http://localhost:4000/api/producto/'+id)
+    .then(res=>res.json())
+    .then(res=>console.log(res))
+    .catch(err=>console.error(err))
+}
+
+//verProductoPorID('63456a8d9163cb9dbbcaa235'); Probamos el metodo con un id        
+
