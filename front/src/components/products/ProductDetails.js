60f7bfef5b3d6e7c1e2132b8b5657421b539b2bd
Henry <111475591+Henryar7@users.noreply.github.com>
2022-10-24 10:05:10 -0500

    hice un cambio en la linea 10 Automovil.jpg

--- front/src/components/products/ProductDetails.js
+++ front/src/components/products/ProductDetails.js
@@ -7,7 +7,7 @@ export const ProductDetails = () => {
         <MetaData title="Agility Alimento Blando Perros"></MetaData>
         <div className='row d-flex justify-content-around'>
             <div className='col-12 col-lg-5 img-fluid' id="imagen_producto">
-                <img src="../../images/productos/agility_blando_perros.png" alt="Imagen_producto" height="450" width="450"></img>
+                <img src="../../images/Automovil.jpg" alt="Imagen_producto" height="450" width="450"></img>
             </div>
 
             <div className='col-12 col-lg-5 mt-5'>
