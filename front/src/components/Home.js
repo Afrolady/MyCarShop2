5f89adec167e8c1d829c57a0be7d08b4761cb31b
Henry <111475591+Henryar7@users.noreply.github.com>
2022-10-25 09:09:28 -0500

    anexe la url al Home.js

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -11,14 +11,14 @@ export const Home = () => {
                     <div className='card p-3 rounded'>
                         <img className='card-img-top mx-auto' src='./images/toyota_prado.jpg' alt='Toyota Prado'></img>
                         <div className='card-body d-flex flex-column'>
-                            <h5 id='titulo_producto'><a href='#'>Toyota Prado</a></h5>
+                            <h5 id='titulo_producto'><a href='localhost:27017/afroshop'>Toyota Prado</a></h5>
                             <div className='rating mt-auto'>
                                 <div className='rating-outer'>
                                     <div className='rating-inner'></div>
                                 </div>
                                 <span id='No_de_opiniones'> 5 reviews</span>
                             </div>
-                            <p className='card-text'>$80.000.000</p><a href='#' id="ver_producto" className='btn btn-block'>
+                            <p className='card-text'>$80.000.000</p><a href='localhost:27017/afroshop' id="ver_producto" className='btn btn-block'>
                                 Ver detalles
                             </a>
                         </div>
