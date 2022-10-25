3473cb77dea620331a006fc840becfd77da1caca
Henry <111475591+Henryar7@users.noreply.github.com>
2022-10-24 22:56:56 -0500

    volvi a redactar Home.js desde cero y lo hice conforme a nuestro proyecto, tambien modifique la linea 11 y 21 del Header.js

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,63 +1,36 @@
-import React, { Fragment, useEffect } from 'react'
-import MetaData from './layout/MetaData'
-import {useDispatch, useSelector} from 'react-redux'
-import { getProducts } from '../actions/productActions'
-import { Link } from 'react-router-dom'
-import { useAlert} from 'react-alert'
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
 
-export const Home = () => {
-    const { loading, productos, error} = useSelector(state=> state.products)
-    const alert= useAlert();
+const Home = props => {
+  return (
+    <Fragment>
+        <h1 id="encabezado_productos">Ultimos Vehiculos</h1>
 
-    const dispatch = useDispatch();
-    useEffect(() => {
-        if (error){
-            return alert.error(error)
-        }
-
-        dispatch(getProducts());
-        alert.success("OK")
-    }, [dispatch])
-
-
-    return (
-        <Fragment>
-            {loading ? <i class="fa fa-refresh fa-spin fa-3x fa-fw"></i> :(
-                <Fragment>
-                    <MetaData title="Lo mejor para tu compañero"></MetaData>
-            <h1 id="encabezado_productos">Ultimos Productos</h1>
-
-            <section id="productos" className='container mt-5'>
-                <div className='row'> 
-                    {productos && productos.map (producto => (
-                        <div key={producto._id} className='col-sm-12 col-md-6 col-lg-3 my-3'>
-                        <div className='card p-3 rounded'>
-                            <img className='card-img-top mx-auto' src={producto.imagen[0].url} alt={producto.imagen[0].public_id}></img>
-                            <div className='card-body d-flex flex-column'>
-                                <h5 id="titulo_producto"><Link to={`/producto/${producto._id}`}>{producto.nombre}</Link></h5>
-                                <div className='rating mt-auto'>
-                                    <div className='rating-outer'>
-                                        <div className='rating-inner' style={{width: `${(producto.calificacion/5)*100}%`}}></div>
-                                    </div>
-                                    <span id="No_de_opiniones"> {producto.numCalificaciones} Reviews</span>
-                                </div>
-                                <p className='card-text'>${producto.precio}</p><Link to={`/producto/${producto._id}`} id="view_btn" className='btn btn-block'>
-                                    Ver detalle
-                                </Link>
+        <section id="productos" className='container mt-5'>
+            <div className='row'>
+                <div className='col-sm-12 col-md-6 col-lg-3 my-'>
+                    <div className='card p-3 rounded'>
+                        <img className='card-img-top mx-auto' src='./images/toyota_prado.jpg' alt='Toyota Prado'></img>
+                        <div className='card-body d-flex flex-column'>
+                            <h5 id='titulo_producto'><a href='#'>Toyota Prado</a></h5>
+                            <div className='rating mt-auto'>
+                                <div className='rating-outer'>
+                                    <div className='rating-inner'></div>
                             </div>
+                            <span id='No_de_opiniones'> 5 reviews</span>
                         </div>
-                    </div>
+                        <p className='card-text'>$80.000.000</p><a href='#' id="ver_producto" className='btn btn-block'>
+                            Ver detalles
+                        </a>
+                    </div> 
+                </div>   
+            </div>
 
-                    ))}
-                    </div>
-            </section>
 
-                </Fragment>
-
-            )}
+        </section>
             
 
-        </Fragment>
-    )
+    </Fragment>
+  )
 }
 export default Home
