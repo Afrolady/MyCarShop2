2c18b5e1efee90efb12f5828db3cf00578780e9a
Leydi Vidal <afroladymersi2012@gmail.com>
2022-11-01 11:51:56 -0500

    Creacion de token en cookies para autenticacion

--- back/controllers/authController.js
+++ back/controllers/authController.js
@@ -1,6 +1,8 @@
 const User = require("../models/auth")
 const ErrorHandler= require("../utils/errorHandler")
 const catchAsyncErrors= require("../middleware/catchAsyncErrors")
+const tokenEnviado = require("../utils/jwtToken");
+
 
 //Registrar un nuevo usuario /api/usuario/registro
 
@@ -17,8 +19,32 @@ exports.registroUsuario= catchAsyncErrors(async (req, res, next) =>{
         }
     })
 
-    res.status(201).json({
-        success:true,
-        user
-    })
+    tokenEnviado(user,201,res)
+})
+
+
+//Iniciar Sesion - Login
+exports.loginUser = catchAsyncErrors(async(req, res, next)=>{
+    const { email, password} =  req.body;
+
+    //revisar si los campos estan completos
+    if (!email || !password){
+        return next(new ErrorHandler("Por favor ingrese email & Contraseña", 400))
+    }
+
+    //Buscar al usuario en nuestra base de datos
+    const user = await User.findOne({email}).select("+password")
+    if(!user){
+        return next(new ErrorHandler("Email o contraseña invalidos", 401))
+    }
+
+    //Comparar contraseñas, verificar si está bien
+    const contrasenaOK= await user.compararPass(password);
+
+    if (!contrasenaOK){
+        return next(new ErrorHandler("Contraseña invalida",401))
+    }
+
+    tokenEnviado(user,200,res)
+
 })
--- back/routes/auth.js
+++ back/routes/auth.js
@@ -3,6 +3,7 @@ const { registroUsuario } = require("../controllers/authController");
 const router= express.Router();
 
 router.route('/usuario/registro').post(registroUsuario)
+router.route('/login').get(loginUser)
 
 
 module.exports= router
