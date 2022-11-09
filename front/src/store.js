40cc28d8e3eed609dce420af04ff62d519f5f344
JuanVimon3 <jvillamonsalve@gmail.com>
2022-11-09 10:13:25 -0500

    se actualiza la clase del 9 de noviembre

--- front/src/store.js
+++ front/src/store.js
@@ -1,11 +1,13 @@
-import { createStore, combineReducers, applyMiddleware} from 'redux';
+import { createStore, combineReducers, applyMiddleware} from 'redux';//aquí debería funcionar createStore
 import thunk from 'redux-thunk';
 import { composeWithDevTools} from 'redux-devtools-extension';
 import { productsReducer, productDetailsReducer } from './reducer/productReducer';
+import { authReducer } from './reducer/userReducer';
 
 const reducer= combineReducers ({
     products:productsReducer,
-    productDetails: productDetailsReducer
+    productDetails: productDetailsReducer,
+    authUser: authReducer
 })
 
 let initialState = {}
