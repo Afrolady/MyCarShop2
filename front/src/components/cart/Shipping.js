243171ea62aa2fee740dcf6f89a2318fd2307e97
Leydi Vidal <afroladymersi2012@gmail.com>
2022-11-18 10:52:48 -0500

    Se modifico el shipping

--- front/src/components/cart/Shipping.js
+++ front/src/components/cart/Shipping.js
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import CheckoutSteps from './CheckOutSteps';
 
 export const Shipping = () => {
-    let Pais = require('./colombia.json');
+    let Pais = require('./Colombia.json');
     const navigate= useNavigate()
     const { shippingInfo } = useSelector(state => state.cart )
 
